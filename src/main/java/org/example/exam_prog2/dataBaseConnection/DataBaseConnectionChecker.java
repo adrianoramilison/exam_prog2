094fedc17f751e4141de24c7dc214c8c8bde2ba2
Adri <mandaramilison19@gmail.com>
2026-08-20 17:18:25 +0300

    database and sql requests

--- src/main/java/org/example/exam_prog2/dataBaseConnection/DataBaseConnectionChecker.java
+++ src/main/java/org/example/exam_prog2/dataBaseConnection/DataBaseConnectionChecker.java
@@ -1,4 +1,28 @@
 package org.example.exam_prog2.dataBaseConnection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
-public class dataBaseCOnnectionChecker {
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DatabaseConnectionChecker implements CommandLineRunner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) {
+        try {
+
+            Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            if (result != null && result == 1) {
+                log.info("Connexion reussie");
+            }
+        } catch (Exception e) {
+            log.error("Impossible de se connecter à PostgreSQL : {}", e.getMessage());
+        }
+    }
 }
+
